507a71f2bbf030f6050c9bfc98f2e7dee028284a
Jayaram <Jayaram@gmail.com>
2025-05-19 23:02:28 +0530

    hello world started

--- roboshop.sh
+++ roboshop.sh
@@ -4,27 +4,40 @@ AMI_ID="ami-09c813fb71547fc4f"
 
 SG_ID="sg-09c5879652d3b90ca"
 
-INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "frontend")  
+INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "frontend")
 
 ZONE_ID="Z07063993EURP4DSS6BCF"
 DOMAIN_NAME="daws1217.site"
 
 for instance in ${INSTANCES[@]}
-
 do
-    
-    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t2.micro --security-group-ids sg-09c5879652d3b90ca --tags-specifications "ResourceType=instance, Tags=[{ Key=Name, value=$instance}]" --query "Instances[0].InstanceId" --output text)
-
-     if[ $instance !="fronend" ]
-
-     then
-
-     IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
-
-     else
-
-         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
+    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t2.micro --security-group-ids sg-01bc7ebe005fb1cb2 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
+    if [ $instance != "frontend" ]
+    then
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
+    else
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
     fi
-    echo "$instance IP address is:: $IP
-
+    echo "$instance IP address: $IP"
 done
+
+
+# for instance in ${INSTANCES[@]}
+
+# do
+    
+#     INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t2.micro --security-group-ids sg-09c5879652d3b90ca --tags-specifications "ResourceType=instance, Tags=[{ Key=Name, value=$instance}]" --query "Instances[0].InstanceId" --output text)
+
+#      if[ $instance !="fronend" ]
+
+#      then
+
+#      IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
+
+#      else
+
+#          IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
+#     fi
+#     echo "$instance IP address is:: $IP
+
+# done
